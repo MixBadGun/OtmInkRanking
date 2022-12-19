8e02fef7a6b72036631fa884ad93df5a549687fb
MixBadGun <1059129006@qq.com>
2022-12-19 08:59:15 +0800

    fixed end

--- danmuku_time.py
+++ danmuku_time.py
@@ -1,8 +1,6 @@
 import logging
-import time
 import requests
 import math
-import danmuku_time
 from lxml import html
 
 def danmuku_time(aid,cid,full_time,sep_time):
@@ -37,7 +35,7 @@ def danmuku_time(aid,cid,full_time,sep_time):
             high_index = high_max_time - 1
     logging.info('获取 av' + str(aid) + ' 视频的起始时间为 ' + str(high_index) + " 秒")
     if full_time - high_index < sep_time:
-        high_index = full_time - sep_time
+        high_index = full_time - sep_time * 1.35
         logging.info('获取 av' + str(aid) + ' 视频的起始时间过短，改为前移')
     if high_index < 0:
         high_index = 0
