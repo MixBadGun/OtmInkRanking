2cdb0634705d0e76bcaf5298a4fa0b6c8ea89d2e
ecral5t <w@ecisr.cc>
2023-07-17 21:12:42 +0800

    apply new data pulling api

--- advanced_main.py
+++ advanced_main.py
@@ -3,7 +3,7 @@ import time
 import logging
 import os
 from config import *
-from get_video_info_score import all_video_info , aid_to_score_norm
+from get_video_info_score import aid_to_score_norm, selected_video_stat, all_video_info
 from function import passMn,short_text
 import shutil
 # 新建文件夹
@@ -35,37 +35,37 @@ with open("custom/data.csv","w",encoding="utf-8-sig",newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(co_header)
     vid_list = []
-    for video in all_video_info:
-        if int(all_video_info[video]["aid"]) in blackArr:
+    for video_aid, video_info in all_video_info.items():
+        video_stat = selected_video_stat.get(video_aid, {})
+        if int(video_aid) in blackArr:
             continue
-        if int(all_video_info[video]["owner"]["mid"]) in adjust_dic:
-            normk = float(adjust_dic[all_video_info[video]["owner"]["mid"]])
+        normk = adjust_dic.get(int(video_info["mid"]), 1)
+        if "copyright" not in video_stat:
+            author_name = f"{video_info['author']} | 未指定"
+        elif str(video_stat["copyright"]) != "1":
+            author_name = f"{video_info['author']} | 转载"
         else:
-            normk = 1
-        if str(all_video_info[video]["copyright"]) not in ["1",1]:
-            author_name = f"{all_video_info[video]['owner']['name']} | 转载"
-        else:
-            author_name = str(all_video_info[video]["owner"]["name"])
-        norm_score = float('%.3f' % (aid_to_score_norm[all_video_info[video]["aid"]] * normk))
+            author_name = str(video_info["author"])
+        norm_score = float('%.3f' % (aid_to_score_norm[video_aid] * normk))
         vid_list.append([
             norm_score,
-            str(all_video_info[video]["aid"]),
-            str(all_video_info[video]["bvid"]),
-            str(all_video_info[video]["cid"]),
-            str(all_video_info[video]["title"]),
+            str(video_aid),
+            str(video_info["bvid"]),
+            "", # 两个新 api 都没返回 cid
+            str(video_info["title"]),
             author_name,
-            str(all_video_info[video]["stat"]["view"]),
-            str(all_video_info[video]["stat"]["like"]),
-            str(all_video_info[video]["stat"]["coin"]),
-            str(all_video_info[video]["stat"]["favorite"]),
-            str(all_video_info[video]["pic"]),
-            time.strftime("%Y/%m/%d %H:%M:%S",time.localtime(int(all_video_info[video]["pubdate"]))),
+            str(video_info["play"]),
+            str(video_stat.get("like", "未取得")),
+            str(video_stat.get("coin", "未取得")),
+            str(video_stat.get("favorites", "未取得")),
+            str(video_info["pic"]),
+            str(video_info["pubdate"]),
             str(normk),
-            str(all_video_info[video]["title"]),
+            str(video_info["title"]),
             '1',
             '',
-            str(all_video_info[video]["copyright"]),
-            str(all_video_info[video]["owner"]["name"])
+            str(video_stat.get("copyright", "未取得")),
+            str(video_info['author'])
         ])
     vid_list = sorted(vid_list,key=lambda x:x[0],reverse=True)
     ranking = 0
--- config_login.py
+++ config_login.py
@@ -1,3 +1,11 @@
+# 如果获取评论时最多只能获取到某个值（比如 20）条评论，
+# 就说明需要登录：在下面填写 cookie
+# 在浏览器中登录 bilibili，然后按 F12 打开开发者工具
+# 方法一 切换到 Network 标签页，刷新页面，点击第一个请求，查看 Request Headers 中的 cookie 字段   
+# 方法二 切换到 Application 标签页，在左侧点击 Cookies，选中 bilibili 域下域名，查看右侧的 cookie 字段
+# 务必不要泄露、公开
+# 详见 https://nemo2011.github.io/bilibili-api/#/get-credential
+
 sessdata   = ""
 bili_jct   = ""
 buvid3     = ""
